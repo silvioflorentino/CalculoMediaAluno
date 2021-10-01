2a40dd0da0992d962fd6d540710e189800b3fffb
silvio florentino <professorsilvioflorentino@yahoo.com.br>
2021-10-01 11:29:00 -0300

    Adicionar arquivos de projeto.

---
+++ CalculoMediaAluno/TelaEntradaDados.Designer.cs
@@ -0,0 +1,294 @@
+﻿
+namespace CalculoMediaAluno
+{
+    partial class TelaEntradaDados
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tbNome = new System.Windows.Forms.TextBox();
+            this.dTPNascimento = new System.Windows.Forms.DateTimePicker();
+            this.nAtiv01 = new System.Windows.Forms.NumericUpDown();
+            this.nMedia = new System.Windows.Forms.NumericUpDown();
+            this.nAtiv04 = new System.Windows.Forms.NumericUpDown();
+            this.nAtiv03 = new System.Windows.Forms.NumericUpDown();
+            this.nAtiv02 = new System.Windows.Forms.NumericUpDown();
+            this.btnMedia = new System.Windows.Forms.Button();
+            this.lBResul = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.nAtiv01)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nMedia)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAtiv04)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAtiv03)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAtiv02)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(113, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculando a Média dos alunos";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 56);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nome:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(394, 56);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Data Nascimento:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(59, 126);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Insira as Notas";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(216, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Atividade 02:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(240, 278);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(54, 20);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Média:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(216, 129);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 20);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Atividade 01:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(216, 226);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(96, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Atividade 04:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(216, 192);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(96, 20);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Atividade 03:";
+            // 
+            // tbNome
+            // 
+            this.tbNome.Location = new System.Drawing.Point(59, 79);
+            this.tbNome.Name = "tbNome";
+            this.tbNome.Size = new System.Drawing.Size(305, 27);
+            this.tbNome.TabIndex = 9;
+            // 
+            // dTPNascimento
+            // 
+            this.dTPNascimento.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dTPNascimento.Location = new System.Drawing.Point(394, 79);
+            this.dTPNascimento.Name = "dTPNascimento";
+            this.dTPNascimento.Size = new System.Drawing.Size(127, 27);
+            this.dTPNascimento.TabIndex = 10;
+            // 
+            // nAtiv01
+            // 
+            this.nAtiv01.Location = new System.Drawing.Point(331, 122);
+            this.nAtiv01.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nAtiv01.Name = "nAtiv01";
+            this.nAtiv01.Size = new System.Drawing.Size(63, 27);
+            this.nAtiv01.TabIndex = 11;
+            this.nAtiv01.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // nMedia
+            // 
+            this.nMedia.Location = new System.Drawing.Point(312, 278);
+            this.nMedia.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nMedia.Name = "nMedia";
+            this.nMedia.Size = new System.Drawing.Size(51, 27);
+            this.nMedia.TabIndex = 12;
+            // 
+            // nAtiv04
+            // 
+            this.nAtiv04.Location = new System.Drawing.Point(331, 218);
+            this.nAtiv04.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nAtiv04.Name = "nAtiv04";
+            this.nAtiv04.Size = new System.Drawing.Size(63, 27);
+            this.nAtiv04.TabIndex = 13;
+            this.nAtiv04.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // nAtiv03
+            // 
+            this.nAtiv03.Location = new System.Drawing.Point(331, 185);
+            this.nAtiv03.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nAtiv03.Name = "nAtiv03";
+            this.nAtiv03.Size = new System.Drawing.Size(63, 27);
+            this.nAtiv03.TabIndex = 14;
+            this.nAtiv03.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // nAtiv02
+            // 
+            this.nAtiv02.Location = new System.Drawing.Point(331, 155);
+            this.nAtiv02.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.nAtiv02.Name = "nAtiv02";
+            this.nAtiv02.Size = new System.Drawing.Size(63, 27);
+            this.nAtiv02.TabIndex = 15;
+            this.nAtiv02.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnMedia
+            // 
+            this.btnMedia.Location = new System.Drawing.Point(426, 278);
+            this.btnMedia.Name = "btnMedia";
+            this.btnMedia.Size = new System.Drawing.Size(94, 29);
+            this.btnMedia.TabIndex = 16;
+            this.btnMedia.Text = "Média";
+            this.btnMedia.UseVisualStyleBackColor = true;
+            this.btnMedia.Click += new System.EventHandler(this.btnMedia_Click);
+            // 
+            // lBResul
+            // 
+            this.lBResul.FormattingEnabled = true;
+            this.lBResul.ItemHeight = 20;
+            this.lBResul.Location = new System.Drawing.Point(59, 318);
+            this.lBResul.Name = "lBResul";
+            this.lBResul.Size = new System.Drawing.Size(451, 224);
+            this.lBResul.TabIndex = 17;
+            // 
+            // TelaEntradaDados
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(574, 563);
+            this.Controls.Add(this.lBResul);
+            this.Controls.Add(this.btnMedia);
+            this.Controls.Add(this.nAtiv02);
+            this.Controls.Add(this.nAtiv03);
+            this.Controls.Add(this.nAtiv04);
+            this.Controls.Add(this.nMedia);
+            this.Controls.Add(this.nAtiv01);
+            this.Controls.Add(this.dTPNascimento);
+            this.Controls.Add(this.tbNome);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "TelaEntradaDados";
+            this.Text = "Média dos Alunos";
+            ((System.ComponentModel.ISupportInitialize)(this.nAtiv01)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nMedia)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAtiv04)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAtiv03)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nAtiv02)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tbNome;
+        private System.Windows.Forms.DateTimePicker dTPNascimento;
+        private System.Windows.Forms.NumericUpDown nAtiv01;
+        private System.Windows.Forms.NumericUpDown nMedia;
+        private System.Windows.Forms.NumericUpDown nAtiv04;
+        private System.Windows.Forms.NumericUpDown nAtiv03;
+        private System.Windows.Forms.NumericUpDown nAtiv02;
+        private System.Windows.Forms.Button btnMedia;
+        private System.Windows.Forms.ListBox lBResul;
+    }
+}
+
---
+++ CalculoMediaAluno/TelaEntradaDados.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalculoMediaAluno
+{
+    public partial class TelaEntradaDados : Form
+    {
+        public TelaEntradaDados()
+        {
+            InitializeComponent();
+        }
+        private decimal MediaAluno (params decimal[] notas)
+        {
+            decimal soma = 0;
+            for(int i=0; i <=notas.GetLength(0) - 1; i++)
+            {
+                soma += notas[i];
+            }
+
+            return soma / notas.Length;
+        }
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+            lBResul.Items.Clear();
+            string nome = tbNome.Text;
+            string nascimento = dTPNascimento.Value.ToString();
+            decimal nota1 = nAtiv01.Value;
+            decimal nota2 = nAtiv02.Value;
+            decimal nota3 = nAtiv03.Value;
+            decimal nota4 = nAtiv04.Value;
+
+            lBResul.Items.Add(
+                "Aluno: " + nome
+                );
+
+            lBResul.Items.Add(
+                "Data Nascimento: " + nascimento
+                );
+            lBResul.Items.Add("Notas das atividades");
+
+            lBResul.Items.Add("Atividade 01: " + nota1);
+            lBResul.Items.Add("Atividade 02: " + nota2);
+            lBResul.Items.Add("Atividade 03: " + nota3);
+            lBResul.Items.Add("Atividade 04: " + nota4);
+            lBResul.Items.Add("Média Final: " + MediaAluno(nota1, nota2, nota3, nota4));
+
+            decimal media = MediaAluno(nota1, nota2, nota3, nota4);
+            // nMedia.Value = media;
+            decimal valormedia = nMedia.Value;
+            if(media >= valormedia)
+            {
+                MessageBox.Show("Aluno Aprovado");
+            }
+            else
+            {
+                MessageBox.Show("Aluno Reprovado");
+            }
+        }
+    }
+}
